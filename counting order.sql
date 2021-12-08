02d540d6a200308a75418808373500a0a554f724
tphan18a3 <93408987+tphan18a3@users.noreply.github.com>
2021-12-08 16:26:04 -0500

    Delete counting order.sql

--- counting order.sql
+++
@@ -1,6 +0,0 @@
-SELECT count(o.order_id), o.person_id,
-		sum(o.total_price), d.delivery_time
-FROM group4campuseats.orders AS o
-JOIN group4campuseats.delivery1 AS d
-	ON o.delivery_id = d.delivery_id
-WHERE person_id = 55;
