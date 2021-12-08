74dffc7d24f1310951d71e6b087fc584df0e6dc0
Mai Lee <93408805+mlee184@users.noreply.github.com>
2021-12-08 08:23:50 -0500

    DataSetCampusEats

---
+++ Campus Eats Script-1.sql
@@ -0,0 +1,4 @@
+use group4campuseats;
+
+CREATE TABLE `group4campuseats`.`delivery1` (`delivery_id` int, `driver_id` int, `vehicle_id` int, `delivery_time` text)
+CREATE DATABASE `group4campuseats` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
